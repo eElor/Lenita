3ce0d4de85fe3ee90c61a5ed61c96839101e32f3
Ernesto Elorduy Vergara <scotbusa@hotmail.com>
2019-09-27 01:07:28 +0200

    prettier message

--- R/packages_lenita.R
+++ R/packages_lenita.R
@@ -3,7 +3,7 @@ packages_lenita <- function(){
   Packs <- c("tidyverse", "writexl", "readxl", "ggbeeswarm", "devtools", "cowplot", "crayon", "broom", "ggrepel")
   for(package in Packs){
     if(package %in% installed.packages()){
-      cat(package, "is already installed")
+      cat(package, "is already installed\n")
       next()
     }
     if(!package %in% installed.packages()){
