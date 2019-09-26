aaa16717ce332387a0d0a5b9f73ae6b310fabad3
Ernesto Elorduy Vergara <scotbusa@hotmail.com>
2019-09-26 23:56:18 +0200

    get_data and norm_distr actualized

---
+++ R/get_data.R
@@ -0,0 +1,55 @@
+
+get_data <- function(Dir=".", FileType="csv", MetaSep="[\\/\\s]", Meta=NULL, SaveMeta=FALSE){
+
+  # get file names
+  FileNames <- list.files(Dir, recursive = TRUE)
+
+  # get metadata as list
+  if (is.null(Meta)){
+    MetaList <- str_remove(FileNames, str_c("\\.", FileType)) %>% str_split(MetaSep)
+  }
+
+  # determine length of metadata coded in file name
+  MetaLengths <- sapply(MetaList, length)
+  MaxMetaLength <- max(MetaLengths)
+
+  # test if meta in file names complete
+  if(!all(MetaLengths==MaxMetaLength)){
+    stop("\n\n!!!\nLength of the metadata (info coded in filename) is not the same for every file.\n",
+         "Expected legth: ", MaxMetaLength, "\n",
+         "Verify files:\n",
+         str_c("\t\t-> '", FileNames[MetaLengths!=MaxMetaLength], "'\n"), "!!!\n\n")
+  }
+
+  # get colnames for metadata
+  MetaColumns <- map_chr(seq_len(MaxMetaLength), ~ {
+      ex <- head(unique(map_chr(MetaList, .x)), 3) # examples of unique values to name
+      ex <- str_c(ex, collapse = " / ")
+      cat(str_c("Provide a name for: ", ex, "\n"))
+      readline(" >>> ")
+    })
+
+  # assign names to MetaList and transform to data.frame
+  Meta <- map(MetaList, ~ set_names(.x, MetaColumns) %>% t %>% as_tibble) %>%
+    bind_rows() %>%
+    mutate(path=FileNames)
+
+  # save Meta as excel if needed
+  if (SaveMeta) writexl::write_xlsx(Meta, file.path(Dir,"../Meta.xlsx"))
+
+  # load data as nested tibbles
+  Data <- mutate(Meta, data=map(path, read.csv, stringsAsFactors=FALSE))
+
+  # which columns to extract
+  Cols <- names(Data$data[[1]])
+  cat("Which columns would you like to keep? (write numbers)\nOptions: \n",
+      map_chr(seq_along(Cols), ~ str_c("\t [", .x, "] ", Cols[.x],"\n")))
+  ColsToKeep <- readline(" >>> ") %>% {eval(parse(text = .))}
+
+  # extract columns
+  Data <- mutate(Data, data=map(data, ~.x[,ColsToKeep]))
+  return(Data)
+
+}
+
+
---
+++ R/norm_distrib.R
@@ -0,0 +1,12 @@
+
+norm_distrib <- function() {
+  norm_dist <- is.na(normality_test$sig)
+  ratio <- paste0(as.character(sum(norm_dist)), "/", length(normality_test$sig), "")
+  if (all(norm_dist)) {
+    cat(crayon::white$bgGreen$bold("\n\n\n\n All groups are normally distributed!   \n   >>> use t.test for inference! JUHU!!!\n\n\n\n\n"))
+  } else if (any(!norm_dist)) {
+    cat(crayon::white$bgMagenta$bold("\n\n\n\n Not all groups are normally distributed! :'(  [", ratio, "] \n   >>> Maybe use a non-parametric test (e. g. Wilcox) \n\n\n\n\n"))
+  } else {
+    cat(crayon::white$bgRed$bold("\n\n\n\n ERROR:                  \n>>> Something went wrong \nwith the normality test! \nCheck that again!!!      \n\n\n\n\n"))
+  }
+}
