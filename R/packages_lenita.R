0ec448aef8d56065132deaba152a80da56df9d2c
Ernesto Elorduy Vergara <scotbusa@hotmail.com>
2019-09-27 01:28:33 +0200

    no sleep

--- R/packages_lenita.R
+++ R/packages_lenita.R
@@ -1,20 +1,24 @@
 
 packages_lenita <- function(){
+
   Packs <- c("tidyverse", "writexl", "readxl", "ggbeeswarm", "devtools", "cowplot", "crayon", "broom", "ggrepel")
+
   for(package in Packs){
+
     if(package %in% installed.packages()){
       cat(package, "is already installed\n")
       next()
     }
+
     if(!package %in% installed.packages()){
       cat(package, "is being installed...")
       install.packages(package, dependencies = TRUE, verbose = FALSE, quiet = TRUE)
-      }
+    }
+
     if(package %in% installed.packages()){
       cat(package, "successfully installed")
     } else {
       warning("\n", package, " could not be installed!!!!\n")
-      Sys.sleep(1)
     }
   }
 }
