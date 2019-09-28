8292363b0e903ed0f2d621f44ab966916ae79488
Ernesto Elorduy Vergara <scotbusa@hotmail.com>
2019-09-28 16:40:39 +0200

    summary_statistics

---
+++ R/summary_statistics.R
@@ -0,0 +1,15 @@
+
+
+summary_statistics <- function(DF){
+  DF %>%
+  filter(!is.na(value)) %>%
+  group_by_at(vars(-value)) %>%
+  summarise(
+    n = n(),
+    mean = mean(value),
+    sd = sd(value),
+    sem = sd/sqrt(n),
+    median = median(value),
+    "25%" = quantile(value, .25),
+    "75%" = quantile(value, .75)
+  )}
