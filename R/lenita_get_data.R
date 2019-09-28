f0b2867d4d671f1e74171ab1220217fbc014a28c
Ernesto Elorduy Vergara <scotbusa@hotmail.com>
2019-09-28 23:27:50 +0200

    _

--- R/lenita_get_data.R
+++ R/lenita_get_data.R
@@ -49,8 +49,8 @@ lenita_get_data <- function(Dir="RAW_DATA", FileType="csv", MetaSep="[\\/\\s]", 
   Data <- mutate(Meta, data=map(path, read_csv, col_types=cols()))
 
   # which columns to extract
+  Cols <- names(Data$data[[1]])
   if(is.null(ColToKeep)){
-    Cols <- names(Data$data[[1]])
     cat("Which column would you like to keep? (write one number)\nOptions: \n",
         map_chr(seq_along(Cols), ~ str_c("\t [", .x, "] ", Cols[.x],"\n")))
     ColToKeep <- readline(" >>> ") %>% {eval(parse(text = .))}
